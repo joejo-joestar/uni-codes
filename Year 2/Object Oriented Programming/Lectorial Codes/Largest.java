d566ccbda4ca910082dbdf3b740943772a755cfe
goofy-ahh-ringtone <115020986+goofy-ahh-ringtone@users.noreply.github.com>
2023-09-05 19:01:47 +0400

    tut

---
+++ Year 2/Object Oriented Programming/Lectorial Codes/Largest.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+class Largest {
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        int num1, num2, num3 = 0;
+        
+        System.out.print("Please enter the first number: ");
+        num1 = inp.nextInt();
+
+        System.out.print("Please enter the second number: ");
+        num2 = inp.nextInt();
+
+        System.out.print("Please ennter the third number: ");
+        num3 = inp.nextInt();
+
+        if(num1 > num2 && num1 > num3) {
+            System.out.println("The Larget Number is: " + num1);
+            
+        }
+
+        else if(num2 > num1 && num2 > num3) {
+            System.out.println("The Larget Number is: " + num2);
+            
+        }
+
+        else if(num3 > num2 && num3 > num1) {
+            System.out.println("The Larget Number is: " + num3);
+            
+        }
+
+        else {
+            System.out.println("All three numbers are equal.");
+            
+        }
+
+    }
+
+}
---
+++ Year 2/Object Oriented Programming/Lectorial Codes/OddEven.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class OddEven {
+	public static void main (String[] args) {
+
+		Scanner inp = new Scanner(System.in);
+
+		int num;
+
+		System.out.print("Please enter a number: ");
+		num = inp.nextInt();
+	
+		if(num % 2 == 0) {
+			System.out.println("The number '" + num + "' is even");
+	
+		}
+
+		else {
+			System.out.println("The number '" + num + "' is odd");
+	
+		}
+
+	}
+}			
