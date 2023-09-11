7da2d384b154a6d6ef0abdc91fe638787b49dce1
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-11 21:29:32 +0400

    Create 04_NOT_Gate_DataFlow.sv

---
+++ Year 2/Digital Design/ Lab Codes [Verilog Programs]/04_NOT_Gate_DataFlow.sv
@@ -0,0 +1,32 @@
+/*
+Code can be found here: https://edaplayground.com/x/gdBP
+*/
+
+//main program
+module NOT_Gate (output reg Z, input A);
+  
+	assign #10 Z = ~A;
+  
+endmodule
+
+
+//testbench
+module tb_NOT_Gate;
+  
+  reg p;
+  wire r;
+  
+  initial begin
+    
+		//$dumpfile ("dump.vcd"); //only needed if using the online compiler
+		//$dumpvars (1, tb_NOT_Gate); //only needed if using the online compiler
+    
+    #000 p = 0;
+    #100 p = 1;
+    #100 $stop;
+    
+  end
+  
+  NOT_Gate U1 (r, p);
+  
+endmodule
