d0b4807f0cf963f826dcc78f4f1d9281edd1a150
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-15 22:19:34 +0400

    Create 07_Twos_Comp.sv

---
+++ Year 2/Digital Design/ Lab Codes [Verilog Programs]/07_Twos_Comp.sv
@@ -0,0 +1,38 @@
+/*
+Code can be found here: https://edaplayground.com/x/gRq2
+*/
+
+//main program
+module Twos_Comp (Z, A);
+
+  output [7:0] Z;
+  input [7:0] A;
+  
+  assign Z = ((~A) + 1);
+
+endmodule
+
+
+//testbench
+module tb_Twos_Comp;
+  
+  reg [7:0] p;
+  wire [7:0] r;
+  
+  initial begin
+    
+    $dumpfile ("dump.vcd");
+    $dumpvars (1, tb_Twos_Comp);
+    
+    //binary number	: 10111011
+    //1's comp 		: 01000100
+    //2's comp 		: 01000101
+    
+    #00 p = 8'b10111011;
+    #800 $stop;
+    
+  end
+  
+  Twos_Comp U1 (r, p);
+  
+endmodule
