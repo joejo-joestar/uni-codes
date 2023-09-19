d6bb5d15beaefa41532e51780c2497ae276f4421
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-19 22:12:58 +0400

    Create E03_Q3_NOT_Gate_w_NAND_Gate.sv

---
+++ Year 2/Digital Design/Lab Codes [Expt Progs]/E03_Q3_NOT_Gate_w_NAND_Gate.sv
@@ -0,0 +1,32 @@
+/*
+Code can be found here: https://edaplayground.com/x/McG_
+*/
+
+//main program
+module NAND_Gate (output reg Z, input A);
+	
+  nand #1 NA1 (Z, A, A);
+  
+endmodule
+
+
+//testbench
+module tb_NAND_Gate;
+  
+  reg p;
+  wire r;
+  
+  initial begin
+    
+    $dumpfile ("dump.vcd");
+    $dumpvars (1, tb_NAND_Gate);
+    
+    #000 p = 0;
+    #100 p = 1;
+    #100 $stop;
+    
+  end
+  
+  NAND_Gate U1 (r, p);
+  
+endmodule
