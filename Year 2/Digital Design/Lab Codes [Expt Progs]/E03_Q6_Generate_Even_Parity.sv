25888d793ab7b149516d602a6a873f5ce55a68d2
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-19 22:17:13 +0400

    Create E03_Q6_Generate_Even_Parity.sv

---
+++ Year 2/Digital Design/Lab Codes [Expt Progs]/E03_Q6_Generate_Even_Parity.sv
@@ -0,0 +1,48 @@
+/*
+Code can be found here: https://edaplayground.com/x/rV_q
+*/
+
+//main program
+module Generate_Parity (z, a, b, c, d);
+  input a, b, c, d;
+  output z;
+  
+  assign z = a ^ b ^ c ^ d;
+  
+endmodule 
+
+
+//testbench
+module tb_Generate_Parity;
+  
+  reg m0, m1, m2, m3;
+  wire P;
+  
+  initial begin
+    
+    $dumpfile ("dump.vcd");
+    $dumpvars (1, tb_Generate_Parity);
+    
+    #000 m0 = 0; m1 = 0; m2 = 0; m3 = 0;
+    #100 m0 = 0; m1 = 0; m2 = 0; m3 = 1;
+    #100 m0 = 0; m1 = 0; m2 = 1; m3 = 0;
+    #100 m0 = 0; m1 = 0; m2 = 1; m3 = 1;
+    #100 m0 = 0; m1 = 1; m2 = 0; m3 = 0;
+    #100 m0 = 0; m1 = 1; m2 = 0; m3 = 1;
+    #100 m0 = 0; m1 = 1; m2 = 1; m3 = 0;
+    #100 m0 = 0; m1 = 1; m2 = 1; m3 = 1;
+    #100 m0 = 1; m1 = 0; m2 = 0; m3 = 0;
+    #100 m0 = 1; m1 = 0; m2 = 0; m3 = 1;
+    #100 m0 = 1; m1 = 0; m2 = 1; m3 = 0;
+    #100 m0 = 1; m1 = 0; m2 = 1; m3 = 1;
+    #100 m0 = 1; m1 = 1; m2 = 0; m3 = 0;
+    #100 m0 = 1; m1 = 1; m2 = 0; m3 = 1;
+    #100 m0 = 1; m1 = 1; m2 = 1; m3 = 0;
+    #100 m0 = 1; m1 = 1; m2 = 1; m3 = 1;
+    #100 $stop;
+    
+  end
+  
+  Generate_Parity U1 (P, m0, m1, m2, m3);
+  
+endmodule
