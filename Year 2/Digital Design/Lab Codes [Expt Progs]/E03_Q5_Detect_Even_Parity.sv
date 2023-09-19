1ee1e51bd1c274fe952da7421f6e9b80dc5ccc08
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-19 22:18:49 +0400

    Update E03_Q5_Detect_Even_Parity.sv

--- Year 2/Digital Design/Lab Codes [Expt Progs]/E03_Q5_Detect_Even_Parity.sv
+++ Year 2/Digital Design/Lab Codes [Expt Progs]/E03_Q5_Detect_Even_Parity.sv
@@ -1,8 +1,44 @@
+/*
+Code can be found here: https://edaplayground.com/x/LqTN
+*/
+
+//main program
 module Detect_Parity (z, a, b, c);
   input a, b, c;
   output z;
   
   assign z = a ^ b ^ c;
   
-  
 endmodule 
+
+
+//testbench
+module tb_Detect_Parity;
+  
+  reg m0, m1, m2;
+  wire P;
+  
+  initial begin
+    
+    $dumpfile ("dump.vcd");
+    $dumpvars (1, tb_Detect_Parity);
+    
+    //binary number	: 10111011
+    //1's comp 		: 01000100
+    //2's comp 		: 01000101
+    
+    #000 m0 = 0; m1 = 0; m2 = 0;
+    #100 m0 = 0; m1 = 0; m2 = 1;
+    #100 m0 = 0; m1 = 1; m2 = 0;
+    #100 m0 = 0; m1 = 1; m2 = 1;
+    #100 m0 = 1; m1 = 0; m2 = 0;
+    #100 m0 = 1; m1 = 0; m2 = 1;
+    #100 m0 = 1; m1 = 1; m2 = 0;
+    #100 m0 = 1; m1 = 1; m2 = 1;
+    #100 $stop;
+    
+  end
+  
+  Detect_Parity U1 (P, m0, m1, m2);
+  
+endmodule
