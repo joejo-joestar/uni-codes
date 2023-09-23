b55fac883e431f916e673147e4bc8d4615f8d501
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-23 12:06:47 +0400

    Create E03_SE01_NOT_w_NOR.sv

---
+++ Year 2/Digital Design/Lab Codes [Self Eval Progs]/E03_SE01_NOT_w_NOR.sv
@@ -0,0 +1,32 @@
+/*
+Code can be found here: https://edaplayground.com/x/McG_
+*/
+
+//main program
+module NOR_Gate (output reg Z, input A);
+	
+  nor #1 NA1 (Z, A, A);
+  
+endmodule
+
+
+//testbench
+module tb_NOR_Gate;
+  
+  reg p;
+  wire r;
+  
+  initial begin
+    
+	  // $dumpfile ("dump.vcd"); //only needed if using the online compiler
+	  // $dumpvars (1, tb_NOR_Gate); //only needed if using the online compiler
+    
+    #000 p = 0;
+    #100 p = 1;
+    #100 $stop;
+    
+  end
+  
+  NOR_Gate U1 (r, p);
+  
+endmodule
