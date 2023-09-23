dc6133ccf3633740ce858bc7401e0897d144b6a0
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-23 13:39:45 +0400

    Create E03_Q2_Hamming_Code.sv

---
+++ Year 2/Digital Design/Lab Codes [Expt Progs]/E03_Q2_Hamming_Code.sv
@@ -0,0 +1,50 @@
+/*
+Code can be found here: https://edaplayground.com/x/Gf_T
+*/
+
+//main program
+module Hamming (P1, P2, P3, m0, m1, m2, m3);
+  input m0, m1, m2, m3;
+  output P1, P2, P3;
+  
+  assign P1 = m0 ^ m1 ^ m3;  
+  assign P2 = m0 ^ m2 ^ m3;  
+  assign P3 = m1 ^ m2 ^ m3;
+  
+endmodule 
+
+
+//testbench
+module tb_Hamming;
+  
+  reg m0, m1, m2, m3;
+  wire P1, P2, P3;
+  
+  initial begin
+    
+    $dumpfile ("dump.vcd");
+    $dumpvars (1, tb_Hamming);
+    
+    #000 m0 = 0; m1 = 0; m2 = 0; m3 =  0;
+    #100 m0 = 0; m1 = 0; m2 = 0; m3 =  1;
+    #100 m0 = 0; m1 = 0; m2 = 1; m3 =  0;
+    #100 m0 = 0; m1 = 0; m2 = 1; m3 =  1;
+    #100 m0 = 0; m1 = 1; m2 = 0; m3 =  0;
+    #100 m0 = 0; m1 = 1; m2 = 0; m3 =  1;
+    #100 m0 = 0; m1 = 1; m2 = 1; m3 =  0;
+    #100 m0 = 0; m1 = 1; m2 = 1; m3 =  1;
+    #100 m0 = 1; m1 = 0; m2 = 0; m3 =  0;
+    #100 m0 = 1; m1 = 0; m2 = 0; m3 =  1;
+    #100 m0 = 1; m1 = 0; m2 = 1; m3 =  0;
+    #100 m0 = 1; m1 = 0; m2 = 1; m3 =  1;
+    #100 m0 = 1; m1 = 1; m2 = 0; m3 =  0;
+    #100 m0 = 1; m1 = 1; m2 = 0; m3 =  1;
+    #100 m0 = 1; m1 = 1; m2 = 1; m3 =  0;
+    #100 m0 = 1; m1 = 1; m2 = 1; m3 =  1;
+    #100 $stop;
+    
+  end
+  
+  Hamming U1 (P1, P2, P3, m0, m1, m2, m3);
+  
+endmodule
