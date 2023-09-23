125f18a0534028629a464b6603704e601534e069
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-23 21:32:41 +0400

    Add files via upload

---
+++ Year 2/Object Oriented Programming/Lectorial Codes/Lect2.java
@@ -0,0 +1,154 @@
+
+// public class Lect2 {
+//     public static void main(String a) {
+//         System.out.println("Hello World");
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main(String[] args) {
+//         final int max = 100;
+//         max++;
+//         System.out.println(max);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int a = 100;
+//         System.out.println(a + "100");
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         String s = "-100";
+//         int i = Integer.parseInt(s);
+//         System.out.println(i);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int x = 10;
+//         String b = Integer.toBinaryString(x);
+//         System.out.println(b);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int x = 10;
+//         System.out.println(Integer.toString(x,2));
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int x = 10;
+//         String o = Integer.toOctalString(x);
+//         System.out.println(o);
+//         System.out.println(Integer.toString(x,8));
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int x = 10;
+//         String h = Integer.toHexString(x);
+//         System.out.println(h);
+//         System.out.println(Integer.toString(x,16));
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         // float x = 238.88;
+//         double y = 0x443;
+//         // byte b = 128;
+//         System.out.println(y);
+//     }
+// }       
+
+        
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int a = 14, b = -14;
+//         System.out.println(a>>2);
+//         System.out.println(b>>1);
+//         System.out.println(a>>>2);
+//         System.out.println(b<<1);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int a = 10, b = 5;
+//         int c = 20 - (++b) - a--;
+//         System.out.println(a+b+c);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int a = 3000, b;
+//         short c;
+//         byte b1 = -50, b2;
+//         long l;        
+        // b = a * b1;  // St 1
+//         c = a * b1;  // St 2
+//         l = a * 25f; // St 3
+//         System.out.println(b +  ", " + c +", " + l);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int a = 0b1010;
+//         System.out.println(a);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         long a = 0b10101010L;
+//         System.out.println(a>>2);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         long salary = 100_000;
+//         System.out.println(salary + 200);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         int i = new Integer(10);
+//         double d = new Double(20.0);
+//         System.out.println(i * d);
+//     }
+// }
+
+
+// public class Lect2 {
+//     public static void main (String[] args) {
+//         boolean b;
+//         System.out.println(b);
+//     }
+// }
