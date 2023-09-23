4c84b4fd757128e09f532930d13e7dd11db5d48d
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-23 12:44:32 +0400

    Update and rename E03_SE06_OR_w_NAND.sv to E03_SE05_OR_w_NAND.sv

--- Year 2/Digital Design/Lab Codes [Self Eval Progs]/E03_SE05_OR_w_NAND.sv
+++ Year 2/Digital Design/Lab Codes [Self Eval Progs]/E03_SE05_OR_w_NAND.sv
@@ -1,17 +1,17 @@
 /*
-Code can be found here: https://edaplayground.com/x/DuiY
+Code can be found here: https://edaplayground.com/x/mkfK
 */
 
 //main program
 module NAND_Gate (Z, A, B);
 
-  output Z;
-  input A, B;
-  wire p;
+    output Z;
+    input A, B;
+    wire p, q;
 
-  nand #1 NA1 (p, A, B);
-  nand #1 NA3 (Z, p, p);
-    
+    nand #1 NA1 (p, A, A);
+    nand #1 NA2 (q, B, B);
+    nand #1 NA3 (Z, p, q);
   
 endmodule
 
@@ -24,8 +24,8 @@ module tb_NAND_Gate;
 
    initial begin
 
-     $dumpfile ("dump.vcd"); //only needed if using the online compiler
-     $dumpvars (1, tb_NAND_Gate); //only needed if using the online compiler
+     // $dumpfile ("dump.vcd"); //only needed if using the online compiler
+     // $dumpvars (1, tb_NAND_Gate); //only needed if using the online compiler
 
      #000 in_1 = 0; in_2 = 0;
      #100 in_1 = 0; in_2 = 1;
