8ab5769fa7ee04312a4fe89bafdf40cb79ee5ada
Joe <144523549+joejo-joestar@users.noreply.github.com>
2023-09-23 12:17:13 +0400

    Create E03_SE05_AND_w_NAND.sv

---
+++ Year 2/Digital Design/Lab Codes [Self Eval Progs]/E03_SE05_AND_w_NAND.sv
@@ -0,0 +1,40 @@
+/*
+Code can be found here: https://edaplayground.com/x/McG_
+*/
+
+//main program
+module NAND_Gate (output reg Z, input A, B);
+
+    output Z;
+    input A, B;
+    wire p;
+
+    nand #1 NA1 (p, A, B);
+    nand #1 NA3 (Z, p, p);
+    
+  
+endmodule
+
+
+//testbench
+module tb_NAND_Gate;
+    
+    reg in_1, in_2;
+    wire op;
+
+    initial begin
+
+        $dumpfile ("dump.vcd"); //only needed if using the online compiler
+        $dumpvars (1, tb_NAND_Gate); //only needed if using the online compiler
+
+        #000 in_1 = 0; in_2 = 0;
+        #100 in_1 = 0; in_2 = 1;
+        #100 in_1 = 1; in_2 = 0;
+        #100 in_1 = 1; in_2 = 1;
+        #100 $stop;
+
+        end
+
+    NAND_Gate U1 (op, in_1, in_2);
+    
+endmodule
