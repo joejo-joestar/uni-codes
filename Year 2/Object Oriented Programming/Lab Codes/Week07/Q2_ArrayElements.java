c26e2f8e08dc23fca199221db638ceaae9ca18a0
Joe <joecn2704+github@gmail.com>
2023-10-11 21:40:55 +0400

    stil no work (sed :disappointed: )

--- Year 2/Object Oriented Programming/Lab Codes/Week07/Q2_ArrayElements.java
+++ Year 2/Object Oriented Programming/Lab Codes/Week07/Q2_ArrayElements.java
@@ -4,32 +4,51 @@
 // incomplete code
 // incomplete code
 
-
 import java.util.*;
 
 class DuplicateChecker {
-    int lim;
-    int[] arr;
-    int[] arrCount;
-    int[] arrDupe;
+    private int[] arr;
+    private int[] arrDupe; // array with duplicated elements
+    private int[] arrCount; // array with num of duplicates
 
     public DuplicateChecker(int[] arr) {
         this.arr = arr;
-        lim = arr.length;
-        arrCount = new int[lim];
-        arrDupe = new int[lim];
+
+    }
+
+    public int[] duplicates() {
+        arrDupe = new int[arr.length];
+        boolean[] flag = new boolean[arr.length];
+        int dupeCount = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (flag[i]) {
+                continue;
+            }
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    arrDupe[dupeCount++] = arr[j];
+                    flag[j] = true;
+
+                }
+
+            }
+
+        }
+        arrDupe = Arrays.copyOfRange(arrDupe, 0, dupeCount);
+        return arrDupe;
 
     }
 
     public int[] duplicatesCount() {
-        int dupeCount = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    dupeCount++;
+        arrCount = new int[arrDupe.length];
+
+        for (int i = 0; i < arrDupe.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (arrDupe[i] == arr[j]) {
+                    arrCount[i]++;
 
                 }
-                arrCount[i] = dupeCount;
 
             }
 
@@ -38,23 +57,22 @@ class DuplicateChecker {
 
     }
 
-    public int[] duplicates() {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[i] == arr[j]) {
-                    arrDupe[i] = arr[j];
-                }
-
-            }
-        }
-        return arrDupe;
-
-    }
-
 }
 
 public class Q2_ArrayElements {
+
+    public static void display(int[] arrDupe, int[] arrCount) {
+        System.out.println("\nThe Duplicate Elements are: ");
+
+        for (int i = 0; i < arrCount.length; i++) {
+            System.out.println("The Element " + "'" + arrDupe[i] + "'" + " has " + arrCount[i] + " Duplicates");
+
+        }
+    }
+
     public static void main(String[] args) {
+        @SuppressWarnings("resource") // to ignore the resource leak warning
+
         Scanner joe = new Scanner(System.in);
 
         System.out.print("Please enter the number of elements: ");
@@ -69,11 +87,10 @@ public class Q2_ArrayElements {
         }
         DuplicateChecker check = new DuplicateChecker(arr);
 
-        int[] arrDupe = new int[lim];
-        int[] arrCount = new int[lim];
+        int[] arrDupe = check.duplicates();
+        int[] arrCount = check.duplicatesCount();
 
-        arrDupe = check.duplicates();
-        arrCount = check.duplicatesCount();
+        display(arrDupe, arrCount);
 
     }
 
