5ab8024fb067a687a68f41653fb3166d2c292649
Joe <joecn2704+github@gmail.com>
2023-10-13 17:39:50 +0400

    :)
    this also doen
    ez qn

---
+++ Year 2/Object Oriented Programming/Lab Codes/Week07/Q3_TimesTable.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Calculate {
+    public static void printTable(int limit) {
+        System.out.println();
+
+        for (int row = 1; row <= limit; row++) {
+            for (int col = 1; col <= 10; col++) {
+                System.out.print("| " + (row * col) + "\t|");
+            }
+            System.out.println();
+        }
+
+    }
+
+}
+
+public class Q3_TimesTable {
+    public static void main(String[] args) {
+        @SuppressWarnings("resource") // to ignore the resource leak warning
+
+        Scanner joe = new Scanner(System.in);
+
+        System.out.print("Please enter the table to print: ");
+        Calculate.printTable(joe.nextInt());
+
+    }
+
+}
