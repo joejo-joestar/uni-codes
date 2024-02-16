9eca27f6e5cde50bd732f19747b1609309f1b6ec
Joe <joecn2704+github@gmail.com>
2024-02-16 19:01:11 +0400

    a

--- Year 2/Database Management/week02/Q1.sql
+++ Year 2/Database Management/week02/Q1.sql
@@ -1,90 +1,18 @@
 USE 20220019db;
 
 CREATE TABLE
-    Admission (
-        E_No INT PRIMARY KEY AUTO_INCREMENT,
-        E_Name VARCHAR(32),
-        Age INT,
-        Department VARCHAR(32),
-        DateofAdm DATE,
-        Fee INT,
-        Sex CHAR(1)
-    );
+    Admission (E_No INT PRIMARY KEY AUTO_INCREMENT, E_Name VARCHAR(32), Age INT, Department VARCHAR(32), DateofAdm DATE, Fee INT, Sex CHAR(1));
 
 INSERT INTO
-    Admission (
-        E_No,
-        E_Name,
-        Age,
-        Department,
-        DateofAdm,
-        Fee,
-        Sex
-    )
+    Admission (E_No, E_Name, Age, Department, DateofAdm, Fee, Sex)
 VALUES
-    (
-        1,
-        'Pankaj',
-        24,
-        'Computer',
-        '2002/10/12',
-        120,
-        'M'
-    ),
-    (
-        2,
-        'Shalini',
-        21,
-        'History',
-        '2012/02/25',
-        200,
-        'F'
-    ),
-    (
-        3,
-        'Sanjay',
-        22,
-        'Hindi',
-        '2010/10/10',
-        300,
-        'F'
-    ),
-    (
-        4,
-        'Sudha',
-        25,
-        'History',
-        '2015/05/30',
-        400,
-        'F'
-    ),
-    (
-        5,
-        'Rakesh',
-        22,
-        'Hindi',
-        '2012/12/12',
-        250,
-        'M'
-    ),
-    (
-        6,
-        'Shakeel',
-        30,
-        'History',
-        '2016/01/03',
-        300,
-        'M'
-    ),
-    (
-        7,
-        'Surya',
-        34,
-        'Computer',
-        '2013/03/30',
-        210,
-        'M'
-    );
+    (1, 'Pankaj', 24, 'Computer', '2002/10/12', 120, 'M'),
+    (2, 'Shalini', 21, 'History', '2012/02/25', 200, 'F'),
+    (3, 'Sanjay', 22, 'Hindi', '2010/10/10', 300, 'F')   ,
+    (4, 'Sudha', 25, 'History', '2015/05/30', 400, 'F')  ,
+    (5, 'Rakesh', 22, 'Hindi', '2012/12/12', 250, 'M')   ,
+    (6, 'Shakeel', 30, 'History', '2016/01/03', 300, 'M'),
+    (7, 'Surya', 34, 'Computer', '2013/03/30', 210, 'M');
 
 SELECT
     *
--- Year 2/Database Management/week02/Q2.sql
+++ Year 2/Database Management/week02/Q2.sql
@@ -1,81 +1,20 @@
 USE 20220019db;
 
 CREATE TABLE
-    pet (
-        P_Name VARCHAR(32) PRIMARY KEY,
-        P_Owner VARCHAR(32),
-        Species VARCHAR(32),
-        Sex CHAR(1),
-        Birth DATE,
-        Death DATE DEFAULT NULL
-    );
+    pet (P_Name VARCHAR(32) PRIMARY KEY, P_Owner VARCHAR(32), Species VARCHAR(32), Sex CHAR(1), Birth DATE, Death DATE DEFAULT NULL);
 
 INSERT INTO
     pet (P_Name, P_Owner, Species, Sex, Birth)
 VALUES
-    (
-        'Fluffy',
-        'Harold',
-        'Cat',
-        'f',
-        '1993-02-04'
-    ),
-    (
-        'Claws',
-        'Gwen',
-        'Cat',
-        'm',
-        '1994-03-17'
-    ),
-    (
-        'Buffy',
-        'Harold',
-        'Dog',
-        'f',
-        '1989-05-13'
-    ),
-    (
-        'Fang',
-        'Benny',
-        'Dog',
-        'm',
-        '1990-08-27'
-    ),
-    (
-        'Bowser',
-        'Diane',
-        'Dog',
-        'm',
-        '1979-08-28'
-    ),
-    (
-        'Chirpy',
-        'Gwen',
-        'Bird',
-        'f',
-        '1998-09-11'
-    ),
-    (
-        'Whistler',
-        'Gwen',
-        'Bird',
-        NULL,
-        '1997-12-09'
-    ),
-    (
-        'Slim',
-        'Benny',
-        'Snake',
-        'm',
-        '1996-04-29'
-    ),
-    (
-        'Puffball',
-        'Diane',
-        'Hamster',
-        'f',
-        '1999-03-30'
-    );
+    ('Fluffy', 'Harold', 'Cat', 'f', '1993-02-04'),
+    ('Claws', 'Gwen', 'Cat', 'm', '1994-03-17'),
+    ('Buffy', 'Harold', 'Dog', 'f', '1989-05-13'),
+    ('Fang', 'Benny', 'Dog', 'm', '1990-08-27'),
+    ('Bowser', 'Diane', 'Dog', 'm', '1979-08-28'),
+    ('Chirpy', 'Gwen', 'Bird', 'f', '1998-09-11'),
+    ('Whistler', 'Gwen', 'Bird', NULL, '1997-12-09'),
+    ('Slim', 'Benny', 'Snake', 'm', '1996-04-29'),
+    ('Puffball', 'Diane', 'Hamster', 'f', '1999-03-30');
 
 UPDATE pet
 SET
